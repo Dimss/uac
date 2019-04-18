0f20e21c97959dd4695e27c85398ef157ee3a059
Dmitry Kartsev <dkartsev@redhat.com>
2019-04-18 16:44:06 +0300

    finish simple flow

--- pkg/k8sclient/rbac.go
+++ pkg/k8sclient/rbac.go
@@ -1,12 +1,13 @@
 package k8sclient
 
 import (
-	"fmt"
 	projectv1 "github.com/openshift/client-go/project/clientset/versioned/typed/project/v1"
 	"github.com/sirupsen/logrus"
 	"github.com/uac/pkg/activedirectory"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	rbacV1 "k8s.io/client-go/kubernetes/typed/rbac/v1"
+	apirbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"os"
 )
@@ -27,11 +28,11 @@ func SetUserRbac(userGroups []activedirectory.UserGroups, userName string) {
 	// If no patch is found, inform in log
 	if len(userOcpProjects) == 0 {
 		logrus.Errorf("No projects matched for user %s in OCP cluster. User groups: %s", userName, userGroups)
-
+		return
 	} else {
-		logrus.Infof("User %s should have access to %s", userName, userOcpProjects)
+		logrus.Infof("User %s should have access to projects: %s", userName, userOcpProjects)
 	}
-
+	getAdminRoleBinding(userOcpProjects, userName)
 }
 
 func matchUserGroupsToOcpNs(userGroups []activedirectory.UserGroups, ocpProjects []string) (userOcpProjects []string) {
@@ -45,39 +46,54 @@ func matchUserGroupsToOcpNs(userGroups []activedirectory.UserGroups, ocpProjects
 	return
 }
 
-func GetRunningPods() {
-	conf := "/Users/dima/.kube/config"
-	config, err := clientcmd.BuildConfigFromFlags("", conf)
-	if err != nil {
-		panic(err.Error())
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-}
-
 func getExistingProjects() (projectsNames []string) {
-	conf := "/Users/dima/.kube/config"
-	config, err := clientcmd.BuildConfigFromFlags("", conf)
+	projectv1Client, err := projectv1.NewForConfig(getClientcmdConfigs())
 	if err != nil {
 		panic(err.Error())
 	}
-
-	projectv1Client, err := projectv1.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
 	projects, err := projectv1Client.Projects().List(metav1.ListOptions{})
 	logrus.Infof("Total projects in OCP cluster: %d", len(projects.Items))
 	for _, project := range projects.Items {
 		projectsNames = append(projectsNames, project.Name)
+	}
+	return
+}
+
+func getAdminRoleBinding(userOcpProjects []string, userName string) {
+	logrus.Info("Getting roles")
+	rbacV1Client, err := rbacV1.NewForConfig(getClientcmdConfigs())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, ocpProject := range userOcpProjects {
+		roleBinding, err := rbacV1Client.RoleBindings(ocpProject).Get("admin", metav1.GetOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		// Create user Subject
+		subject := apirbacv1.Subject{
+			Kind:      "User",
+			APIGroup:  "rbac.authorization.k8s.io",
+			Name:      userName,
+			Namespace: "",
+		}
+		// Append user Subject to RoleBinding subject slice
+		roleBinding.Subjects = append(roleBinding.Subjects, subject)
+		roleBinding, err = rbacV1Client.RoleBindings(ocpProject).Update(roleBinding)
+		if err != nil {
+			panic(err.Error())
+		}
+		logrus.Info(roleBinding)
+	}
+
+}
+
+func getClientcmdConfigs() (config *rest.Config) {
+	conf := "/Users/dima/.kube/config"
+	config, err := clientcmd.BuildConfigFromFlags("", conf)
+	if err != nil {
+		panic(err.Error())
 	}
 	return
 }
--- pkg/oauthtokenwebhook/handler.go
+++ pkg/oauthtokenwebhook/handler.go
@@ -10,7 +10,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-
 	"net/http"
 )
 
